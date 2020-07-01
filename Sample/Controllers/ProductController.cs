dc574323b9f9e1923acfca9b8496c203f7871b0e
Galip ÇEVRİK <galip.cevrik@primeapps.io>
2020-07-01 19:43:46 +0300

    fixed

--- Sample/Controllers/ProductController.cs
+++ Sample/Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json.Linq;
+using Sample.Models;
 
 namespace Sample.Controllers
 {
@@ -12,26 +13,24 @@ namespace Sample.Controllers
     public class ProductController : BaseController
     {
         [HttpPost, Route("price_convert")]
-        [ProducesResponseType(typeof(JObject), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProductPrice), StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public IActionResult PriceConvert([FromBody]string currency, [FromBody] int price)
+        public IActionResult PriceConvert([FromBody]ProductPrice product)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
 
-            var data = new JObject();
+            var data = new ProductPrice();
 
-            if (currency.ToLower() == "dollar")
-            {
-                data["rate"] = 6.80;
-                data["price"] = price * 6.80;
-            }
-            
-            if (currency.ToLower() == "euro")
-            {
-                data["rate"] = 7.80;
-                data["price"] = price * 7.80;
-            }
+            if (product.Currency.ToLower() == "dollar (usa)")
+                data.RateOfExchange = product.RateOfExchange <= 0 ? 6.80 : product.RateOfExchange;
+
+
+            if (product.Currency.ToLower() == "euro")
+                data.RateOfExchange = product.RateOfExchange <= 0 ? 6.80 : product.RateOfExchange;
+
+            data.Price = product.Price * data.RateOfExchange;
+            product.Currency = "Turkish Liras";
 
             return Ok(data);
         }
---
+++ Sample/Models/ProductPrice.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Models
+{
+    public class ProductPrice
+    {
+        [JsonProperty("rate_of_exchange")]
+        public double RateOfExchange { get; set; }
+
+        [JsonProperty("currency")]
+        public string Currency { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+    }
+}
