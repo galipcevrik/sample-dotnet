fd7790e919ecf87f4ea8a796a0166741fc6c542b
Galip ÇEVRİK <galip.cevrik@primeapps.io>
2020-07-01 18:25:54 +0300

    price convert api added

---
+++ Sample/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace Sample.Controllers
+{
+    [Route("api/product")]
+    public class ProductController : BaseController
+    {
+        [HttpGet, Route("price_convert")]
+        [ProducesResponseType(typeof(JObject), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult PriceConvert(string currency, int price)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var data = new JObject();
+
+            if (currency.ToLower() == "dollar")
+            {
+                data["rate"] = 6.80;
+                data["price"] = price * 6.80;
+            }
+            
+            if (currency.ToLower() == "euro")
+            {
+                data["rate"] = 7.80;
+                data["price"] = price * 7.80;
+            }
+
+            return Ok(data);
+        }
+    }
+}
